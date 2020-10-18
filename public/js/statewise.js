3268f6d0ba3acf94255b0052e7a587ca12c6459f
Charles Roh <chuckyroh@gmail.com>
2020-10-18 15:45:00 -0700

    add sorting and search to state table

--- public/js/statewise.js
+++ public/js/statewise.js
@@ -1,42 +1,99 @@
-let api = 'https://api.covid19india.org/data.json';
+// main table view model
+var tableViewModel = function(){
+    return {
+        // data variables
+        api: 'https://api.covid19india.org/data.json',
+        data: [],
+        states: [],
+        searchInput: '',
+        sortAsc: false,
+        sortField: 'confirmed',
+        // initializes the retrieved data
+        getData() {
+            fetch(this.api)
+            .then(response => {
+                let data = response.json()
+                return data
+            })
+            .then(data => {
+                // console.log(data)
+                this.data = data
+                this.states = data.statewise.slice()
 
-function get(){
-    fetch(api)
-    .then(function(response){
-        let data = response.json();
-        return data
-    })
+                // remove loader
+                var preloader = document.getElementById('loader')
+                preloader.style.display = 'none'
 
-    .then(function(data){
-        var tabval = document.getElementById('tbval');
-        var r = 1;
-        for(var i = 1; i<= (data['statewise'].length); i++ ){
-            var x = tabval.insertRow();
-            x.insertCell(0);
-            tabval.rows[r].cells[0].innerHTML = data['statewise'][i].state;
-            tabval.rows[r].cells[0].style.color = '#000';
+                // set to default sort
+                this.sortRow(this.sortField, true)
+            })
+        },
+        // sorts the row when clicking on header
+        // - rowField is the field to change to
+        // - noSortChange is there is no sorting change like on reset
+        sortRow(rowField, noSortChange) {
+            // change field for ascending sort if same field
+            if(this.sortField === rowField && !noSortChange) this.sortAsc = !this.sortAsc
 
-            x.insertCell(1);
-            tabval.rows[r].cells[1].innerHTML = data['statewise'][i].confirmed;
-            tabval.rows[r].cells[1].style.color = '#000';
+            var sorted
+            if(rowField === 'state'){
+                sorted = this.states.sort((a, b) => {
+                    var aName = a.state.toUpperCase()
+                    var bName = b.state.toUpperCase()
 
-            x.insertCell(2);
-            tabval.rows[r].cells[2].innerHTML = data['statewise'][i].active;
-            tabval.rows[r].cells[2].style.color = '#000';
+                    // direction of sort changes when clicking on the same field
+                    if(this.sortAsc){
+                        if(aName < bName) return -1
+                        if(aName > bName) return 1
+                        
+                    } else {
+                        if(aName > bName) return -1
+                        if(aName < bName) return 1
+                    }
 
-            x.insertCell(3);
-            tabval.rows[r].cells[3].innerHTML = data['statewise'][i].recovered;
-            tabval.rows[r].cells[3].style.color = '#000';
+                    return 0
 
-            x.insertCell(4);
-            tabval.rows[r].cells[4].innerHTML = data['statewise'][i].deaths;
-            tabval.rows[r].cells[4].style.color = '#000';
+                })
+                
+            } else {
+                sorted = this.states.sort((a, b) => {
+                    // direction of sort changes when clicking on the same field
+                    var aNum = parseInt(a[rowField]) 
+                    var bNum = parseInt(b[rowField]) 
+                    if(this.sortAsc){
+                        if(aNum < bNum) return -1
+                        if(aNum > bNum) return 1
+                    } else {
+                        if(aNum > bNum) return -1
+                        if(aNum < bNum) return 1
+                    }
+                    return 0
+                })
+            }
 
+            // set new current sort field
+            this.sortField = rowField
+            // set new data
+            this.states = sorted
+        },
+        // updates the search with the input, called on input change
+        updateSearch(event) {
+            var pattern = new RegExp(this.searchInput.toUpperCase(), 'g')
+            var filtered
+            filtered = this.data.statewise.slice().filter(row => pattern.test(row.state.toUpperCase()))
 
-            r++;
+            this.states = filtered
+            this.sortRow(this.sortField, true)
+        },
+        // resets the search to whatever is currently set
+        resetSearch(event) {
+            this.states = this.data.statewise.slice()
+            this.sortRow(this.sortField, true)
+            this.searchInput = ''
         }
-    })
-    var preloader = document.getElementById('loader');
-    preloader.style.display = 'none';
+    }
 }
 
+function get(){
+    // nothing needed
+}
